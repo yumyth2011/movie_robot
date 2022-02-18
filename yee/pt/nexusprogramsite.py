6e32f5602b4aaedda8149bb0b2766027e0d48202
Miniers <m@lk.mk>
2022-02-18 23:27:59 +0800

    添加 chdbits pt站支持

--- yee/pt/nexusprogramsite.py
+++ yee/pt/nexusprogramsite.py
@@ -99,7 +99,7 @@ class NexusProgramSite(PTSite, metaclass=ABCMeta):
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             'accept-encoding': 'gzip, deflate, br',
             'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
-            'referer': 'https://hdsky.me/torrents.php',
+            'referer': self.get_site() + '/torrents.php',
             'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36'
         }, cookies=self.cookies, allow_redirects=False)
         if r.status_code != 200 and r.status_code != 302:
